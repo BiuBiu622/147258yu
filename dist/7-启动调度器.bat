24b346d7834a4661c1a8265a0ace50ac1e5a457b
unknown <a1163461609@163.com>
2026-02-07 13:37:03 +0800

    增加重启调度器功能   增加暂停功能

--- dist/7-启动调度器.bat
+++ dist/7-启动调度器.bat
@@ -7,9 +7,12 @@ echo ========================================
 echo 启动任务调度器
 echo ========================================
 echo.
+:start
 echo 调度器正在启动...
 echo 按 Ctrl+C 可以停止调度器
 echo.
 node "server\scheduler.js"
 echo.
-pause
+echo 调度器已停止，3秒后自动重启...
+timeout /t 3 >nul
+goto start
